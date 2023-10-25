e7aef955757777e5052787b1d2ed5c7b2a8b85a1
Chris Mountzouris <30795982+mountzou@users.noreply.github.com>
2023-10-25 16:29:22 +0300

    Updated front-page

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import requests
+from datetime import datetime, timedelta
 
-# Sample data
-data = {
-    'Date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04'],
-    'Value': [10, 20, 30, 40]
-}
-df = pd.DataFrame(data)
+import requests
 
-# Streamlit app
-st.title('Data Visualization with Streamlit')
+API_KEY = '53c365c5-8e53-4583-acf8-e9c37e6f00bd'
+MAC_ADDRESS = 'D2:7E:14:56:24:41'
 
-# Display data in a table
-st.write(df)
 
-# Display data in a line chart
-fig = px.line(df, x='Date', y='Value', title='Line Chart Visualization')
-st.plotly_chart(fig)
+def fetch_data(start_date, end_date):
+    url = 'https://api.atmotube.com/api/v1/data'
+    params = {
+        'api_key': API_KEY,
+        'mac': MAC_ADDRESS,
+        'limit': 100,
+        'offset': 0,
+        'order': 'desc',
+        'start_date': start_date,
+        'end_date': end_date,
+        'format': 'json'
+    }
 
-# Display data in a bar chart
-fig2 = px.bar(df, x='Date', y='Value', title='Bar Chart Visualization')
-st.plotly_chart(fig2)
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error(f"Error fetching data: {response.text}")
+        return None
+
+
+def main():
+    st.title("ATMOTube Data Fetcher")
+
+    today = datetime.today().date()
+    day_before = today - timedelta(days=1)
+
+    default_end_date = today.strftime('%Y-%m-%d')
+    default_start_date = day_before.strftime('%Y-%m-%d')
+
+    col1, col2 = st.columns(2)
+    start_date_input = col1.date_input('Start Date', day_before)
+    end_date_input = col2.date_input('End Date', today)
+
+    if st.button("Fetch Data"):
+        data = fetch_data(start_date_input.strftime('%Y-%m-%d'), end_date_input.strftime('%Y-%m-%d'))
+        if data and "data" in data and "items" in data["data"]:
+            st.table(data["data"]["items"])
+
 
 if __name__ == '__main__':
-    st.write("This is a basic Streamlit app for data visualization.")
+    main()
